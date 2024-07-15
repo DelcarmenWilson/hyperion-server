7dca4c6def4a1062740d7f6a9f835e9a156d40a4
Wilson_Dell_Desk <Wilson_Dell_Desk@DESKTOP-7AT2JED>
2024-07-14 23:16:26 -0400

    add cors

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,7 @@ application.use(express.json());
 /** Rules of our API */
 application.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     if (req.method == 'OPTIONS') {
